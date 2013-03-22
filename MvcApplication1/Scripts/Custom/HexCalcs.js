0608403bb746be2649c3d357314f22fba42db37a
William Getz <willegetz@gmail.com>
2013-03-22 08:24:50 -0700

    Adding mvc application

---
+++ MvcApplication1/Scripts/Custom/HexCalcs.js
@@ -0,0 +1,203 @@
+﻿HexCalcApp.HexCalcViewModel = function (initialMeasure) {
+	var self = this;
+
+	self.derivedSideLength = ko.observable(initialMeasure);
+	self.currentCalcType = ko.observable();
+
+	self.givenApothem = ko.observable();
+	self.calcApothem = ko.computed(function () {
+		if (self.currentCalcType() !== 'apothem') {
+			self.givenApothem(toFixed(((Math.sqrt(3) / 2) * self.derivedSideLength()), 4));
+			return self.givenApothem();
+		} else {
+			return self.givenApothem();
+		}
+	});
+	
+	self.givenPerimeter = ko.observable();
+	self.calcPerimeter = ko.computed(function () {
+		if (self.currentCalcType() !== 'perimeter') {
+			self.givenPerimeter(toFixed((self.derivedSideLength() * 6), 4));
+			return self.givenPerimeter();
+		} else {
+			return self.givenPerimeter();
+		}
+	});
+	
+	self.givenArea = ko.observable();
+	self.calcArea = ko.computed(function () {
+		if (self.currentCalcType() !== 'area') {
+			self.givenArea(toFixed((((3 * Math.sqrt(3)) / 2) * Math.pow(self.derivedSideLength(), 2)), 4));
+			return self.givenArea();
+		} else {
+			return self.givenArea();
+		}
+	});
+	
+	self.givenVtoV = ko.observable();
+	self.calcVtoV = ko.computed(function () {
+		if (self.currentCalcType() !== 'VtoV') {
+			self.givenVtoV(toFixed((2 * self.derivedSideLength()), 4));
+			return self.givenVtoV();
+		} else {
+			return self.givenVtoV();
+		}
+	});
+	
+	self.givenCtoV = ko.observable();
+	self.calcCtoV = ko.computed(function () {
+		if (self.currentCalcType() !== 'CtoV') {
+			self.givenCtoV(toFixed(self.derivedSideLength(), 4));
+			return self.givenCtoV();
+		} else {
+			return self.givenCtoV();
+		}
+	});
+	
+	self.givenSide = ko.observable();
+	self.calcSide = ko.computed(function () {
+		if (self.currentCalcType() !== 'side') {
+			self.givenSide(toFixed(self.derivedSideLength(), 4));
+			return self.givenSide();
+		}else {
+			return self.givenSide();
+		}
+	});
+	
+	self.givenFtoF = ko.observable();
+	self.calcFtoF = ko.computed(function () {
+		if (self.currentCalcType() !== 'FtoF') {
+			self.givenFtoF(toFixed((2 * ((Math.sqrt(3) / 2) * self.derivedSideLength())), 4));
+			return self.givenFtoF();
+		} else {
+			return self.givenFtoF();
+		}
+	});
+	
+	self.apothemEdit = ko.observable(false);
+	self.perimeterEdit = ko.observable(false);
+	self.areaEdit = ko.observable(false);
+	self.vToVEdit = ko.observable(false);
+	self.cToVEdit = ko.observable(false);
+	self.sideEdit = ko.observable(false);
+	self.fToFEdit = ko.observable(false);
+	
+	self.edit = function (data, event) {
+		self.currentCalcType(data);
+		
+		if (data === 'apothem') {
+			self.apothemEdit(true);
+			return;
+		}
+		if (data === 'perimeter') {
+			self.perimeterEdit(true);
+			return;
+		}
+		if (data === 'area') {
+			self.areaEdit(true);
+			return;
+		}
+		if (data === 'VtoV') {
+			self.vToVEdit(true);
+			return;
+		}
+		if (data === 'CtoV') {
+			self.cToVEdit(true);
+			return;
+		}
+		if (data === 'side') {
+			self.sideEdit(true);
+			return;
+		}
+		if (data === 'FtoF') {
+			self.fToFEdit(true);
+			return;
+		}
+	};
+	
+	self.givenApothem.subscribe(function (entry) {
+		if (self.currentCalcType() === 'apothem') {
+			self.derivedSideLength(((2 * entry) / 3) * Math.sqrt(3));
+		}
+	});
+	
+	self.givenArea.subscribe(function (entry) {
+		if (self.currentCalcType() === 'area') {
+			self.derivedSideLength(Math.sqrt(2 * entry) * Math.sqrt(Math.sqrt(3)) / 3);
+		}
+	});
+	
+	self.givenPerimeter.subscribe(function (entry) {
+		if (self.currentCalcType() === 'perimeter') {
+			self.derivedSideLength(entry / 6);
+		}
+	});
+	
+	self.givenVtoV.subscribe(function (entry) {
+		if (self.currentCalcType() === 'VtoV') {
+			self.derivedSideLength(entry / 2);
+		}
+	});
+	
+	self.givenCtoV.subscribe(function (entry) {
+		if (self.currentCalcType() === 'CtoV') {
+			self.derivedSideLength(entry);
+		}
+	});
+	
+	self.givenSide.subscribe(function (entry) {
+		if (self.currentCalcType() === 'side') {
+			self.derivedSideLength(entry);
+		}
+	});
+	
+	self.givenFtoF.subscribe(function (entry) {
+		if (self.currentCalcType() === 'FtoF') {
+			self.derivedSideLength((entry / 3) * Math.sqrt(3));
+		}
+	});
+	
+	// From: http://stackoverflow.com/a/2909252
+	function toFixed(value, precision) {
+		var precision = precision || 0,
+		neg = value < 0,
+		power = Math.pow(10, precision),
+		value = Math.round(value * power),
+		integral = String((neg ? Math.ceil : Math.floor)(value / power)),
+		fraction = String((neg ? -value : value) % power),
+		padding = new Array(Math.max(precision - fraction.length, 0) + 1).join('0');
+
+		return precision ? integral + '.' + padding + fraction : integral;
+	}
+};
+
+// From: http://stackoverflow.com/a/12356271
+(function () {
+	var valueHandler = ko.bindingHandlers.value;
+	var getInjectValueUpdate = function (allBindingsAccessor) {
+		var AFTERKEYDOWN = 'afterkeydown';
+		return function () {
+			var allBindings = ko.utils.extend({}, allBindingsAccessor()),
+                valueUpdate = allBindings.valueUpdate;
+
+			if (valueUpdate === undefined) {
+				return ko.utils.extend(allBindings, { valueUpdate: AFTERKEYDOWN });
+			} else if (typeof valueUpdate === 'string' && valueUpdate !== AFTERKEYDOWN) {
+				return ko.utils.extend(allBindings, { valueUpdate: [valueUpdate, AFTERKEYDOWN] });
+			} else if (typeof valueUpdate === 'array' && ko.utils.arrayIndexOf(valueUpdate, AFTERKEYDOWN) === -1) {
+				valueUpdate = ko.utils.arrayPushAll([AFTERKEYDOWN], valueUpdate);
+				return ko.utils.extend(allBindings, { valueUpdate: valueUpdate });
+			}
+			return allBindings;
+		};
+	};
+	ko.bindingHandlers.value = {
+		'init': function (element, valueAccessor, allBindingsAccessor) {
+			allBindingsAccessor = getInjectValueUpdate(allBindingsAccessor);
+			return valueHandler.init(element, valueAccessor, allBindingsAccessor);
+		},
+		'update': valueHandler.update
+	};
+}());
+
+ko.applyBindings(new HexCalcApp.HexCalcViewModel(0));
---
+++ MvcApplication1/Scripts/Custom/HexagonDrawing.js
@@ -0,0 +1,9 @@
+﻿// Given a length, draw connecting lines to form the hexagon
+// The hexagon will be a flat
+// Point 1 will be the upper right vertex, which is one half the side length.
+// Point 2 will be one side length horzontall from Point 1
+// Point 3 will be One Apothem down and one half side length right of Point 2
+// Point 4 will be one half side length left and one Apothem down from Point 3
+// Point 5 will be one side length left of Point 4
+// Point 6 will be one Apothem high and one half side length left of Point 5
+// Connect to Point 1 will be one half side length right and one Apothem high from Point 6
