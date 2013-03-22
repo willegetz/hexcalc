5b017b66d9eb38bcbd36bd021ae3ee07faff7391
William Getz <willegetz@gmail.com>
2013-03-22 08:46:26 -0700

    Added required dll and namespace updates.

--- MvcApplication1/App_Start/WebApiConfig.cs
+++ MvcApplication1/App_Start/WebApiConfig.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Http;
+﻿using System.Web.Http;
 
-namespace MvcApplication1
+namespace MvcApplication1.App_Start
 {
 	public static class WebApiConfig
 	{
