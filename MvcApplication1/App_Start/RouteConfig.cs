b44b186d9503ad4cc8250dcbe957f0fe8a101c12
William Getz <willegetz@gmail.com>
2013-03-24 21:35:21 -0700

    Small reorg, added basic about section.

--- MvcApplication1/App_Start/RouteConfig.cs
+++ MvcApplication1/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace MvcApplication1.App_Start
 			routes.MapRoute(
 				name: "Default",
 				url: "{controller}/{action}/{id}",
-				defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+				defaults: new { controller = "Home", action = "HexagonCalculator", id = UrlParameter.Optional }
 			);
 		}
 	}
--- MvcApplication1/Controllers/HomeController.cs
+++ MvcApplication1/Controllers/HomeController.cs
@@ -1,20 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace MvcApplication1.Controllers
 {
     public class HomeController : Controller
     {
-        //
-        // GET: /Home/
-
-        public ActionResult Index()
-        {
-            return View("_index");
-        }
-
+		public ActionResult HexagonCalculator()
+		{
+			return View("HexagonCalculator");
+		}
     }
 }
